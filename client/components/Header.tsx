256643eeea1b4463c94f400dff6f558a20bdaf63
MrFruchard <Romain2702@hotmail.fr>
2025-03-19 13:01:52 +0100

    Test implementation login

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -7,7 +7,7 @@ export function Header(): React.ReactElement {
     return (
         <div className="flex fixed justify-between items-center px-4 py-3 border-b border-gray-800">
             <div className="flex items-center">
-                <h1 className="text-xl font-bold text-white">ACTU / FEED</h1>
+                <h1 className="text-xl font-bold">ACTU / FEED</h1>
             </div>
 
             <Button className="bg-blue-100 text-navy-900 hover:bg-blue-200">
